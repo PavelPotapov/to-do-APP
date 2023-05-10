5f7bd69035b270f1ba590891c2b2a480d800be8c
PavelPotapov <pavelpotapovv96@gmail.com>
2023-05-10 17:56:35 +0500

    Добавил middleware и asyncHandler. Нарочно создана ошибка в authUser controller

--- app/auth/auth.controller.js
+++ app/auth/auth.controller.js
@@ -1,10 +1,16 @@
 //@desc Auth user
 //@route POST /api/auth/login
 //@access Public
+import asyncHandler from 'express-async-handler'
 
-import { prisma } from "../prisma.js";
+import { prisma } from '../prisma.js'
 
-export const authUser = async (req, res) => {
-    const user = await prisma.user.findMany()
+
+export const authUser = asyncHandler(async (req, res) => {
+    const user = await prisma.user.findMany({
+        where: {
+            password1: '123'
+        }
+    })
     res.json({message:user});
-}  
+})
--- server.js
+++ server.js
@@ -8,6 +8,7 @@ import morgan from 'morgan';
 
 import authRoutes from './app/auth/auth.routes.js';
 import { prisma } from './app/prisma.js';
+import { errorHandler, notFound } from './app/middleware/error.middleware.js';
 
 
 dotenv.config()
@@ -25,6 +26,9 @@ async function main() {
 	app.use(express.json()) 
 	app.use('/api/auth', authRoutes)
 
+	app.use(notFound)
+	app.use(errorHandler)
+	
 	const PORT = process.env.PORT || 5000
 
 	app.listen(
